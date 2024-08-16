90e90b82061b6a76a038748afa1b7fe3401ce217
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2024-08-16 18:53:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ if st.button("start Recording"):
 if st.button("ASK"):
     st.write("Listening, speak after 2 seconds...")
     time.sleep(2)  # Allow time for user to prepare
-    # text = recognize_speech()
-    # st.write("You said:", text)
+    text = recognize_speech()
+    st.write("You said:", text)
     # words = process_question(text)
     # st.write("Processed words:", words)
     # ans = query_json(st.session_state.detected_objects, words)
